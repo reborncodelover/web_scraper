0c4f030112a3a28bc35e6a6c6140e0a8f0cc46b7
Reborn Code Lover <reborncodelover@gmail.com>
2019-02-01 14:18:17 -0800

    Updated the comments to understand code better

--- get_urls.py
+++ get_urls.py
@@ -3,10 +3,14 @@ import requests
 from bs4 import BeautifulSoup
 
 all_charities = []
+# Scrape from the archive snapshot rather than live Charity Navigator site
 starting_url = "https://web.archive.org/web/20160705164956/http://www.charitynavigator.org/index.cfm?bay=search.alpha&ltr=1"
+
 top_page = requests.get(starting_url)
 top_soup = BeautifulSoup(top_page.text, 'html.parser')
-for letter in top_soup.find(id='maincontent2').find('p').find_all('a'): # loop through every alphabet letter shown in the top nav link
+
+# loop through every alphabet letter shown in the top nav link
+for letter in top_soup.find(id='maincontent2').find('p').find_all('a'): 
 	base_url = starting_url[:-1]
 	starting_url = base_url + letter.text
 	print ("Getting urls of charities starting with the letter: '" , letter.text, "'")
@@ -14,7 +18,10 @@ for letter in top_soup.find(id='maincontent2').find('p').find_all('a'): # loop t
 	page = requests.get(starting_url)
 	soup = BeautifulSoup(page.text, 'html.parser')
 
-	charities_section = soup.find(id= 'maincontent2') # section on the page with the list of charities that start with current letter
+	# section on the page with the list of charities that start with current letter
+	charities_section = soup.find(id= 'maincontent2') 
+
+	# drop both top and bottom nav links
 	top_nav_links = charities_section.find('p')
 	top_nav_links.decompose() # drop the top nav links
 	bottom_nav_links = charities_section.find('p')
@@ -26,7 +33,9 @@ for letter in top_soup.find(id='maincontent2').find('p').find_all('a'): # loop t
 		item = charity.get('href')
 		charity_url = item
 		charity_id = item[item.find('orgid=')+len('orgid='):]
-		charity_name = charity.contents[0] # alternately you could use charity.text to get charity name
+		charity_name = charity.contents[0] 
+		# Hint: alternately you could use charity.text to get charity name
+
 		all_charities.append([charity_id, charity_name, charity_url])
 		#print (charity_id, " : ", charity_name)
 
